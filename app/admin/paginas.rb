3bda7c7e9bbd4025eb4c21a2ebe3c61210a99bf6
Julio Protzek <julioprotzek@gmail.com>
2012-06-22 19:06:44 -0300

    melhorias no admin de pagina

--- app/admin/paginas.rb
+++ app/admin/paginas.rb
@@ -1,6 +1,15 @@
 ActiveAdmin.register Pagina do
   form partial: 'form'
   
+  filter :titulo
+  filter :conteudo
+  
+  index do
+    column :id
+    column :titulo
+    default_actions
+  end
+  
   show do
     render partial: 'show'
   end
