798f67b2370870151de39239c2d27fa12665342d
Julio Protzek <julioprotzek@gmail.com>
2012-06-22 20:16:33 -0300

    devise bugfix

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,12 +1,13 @@
 module DeviseHelper
-  def devise_error_messages!(user)
-    resource = user if user.present?
-    return "" if resource.errors.empty?
+  def devise_error_messages!(user=nil)
+    user_resource = user.present? ? user : resource
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    return "" if user_resource.errors.empty?
+
+    messages = user_resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
+                      :count => user_resource.errors.count,
+                      :user_resource => user_resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div id="error_explanation">
