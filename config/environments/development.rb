f9aecf2a49c8decab03642615c0e91d6f4c7761a
Renan Mendes Carvalho <aitherios@gmail.com>
2013-04-17 18:17:39 -0300

    improving unicorn logging in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,16 +4,18 @@ $stdout.sync = true
 AitheriosStartaeBootstrap::Application.configure do
   config.cache_classes = false
   config.whiny_nils = true
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
   config.action_dispatch.best_standards_support = :builtin
   config.active_record.mass_assignment_sanitizer = :strict
   config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.assets.compress = false
   config.assets.debug = true
   Slim::Engine.set_default_options pretty: true
-  Slim::Engine.set_default_options :format => :html5
-  config.action_mailer.default_url_options = { :host => 'localhost:5000' }
+  Slim::Engine.set_default_options format: :html5
+  config.action_mailer.default_url_options = { host: 'localhost:5000' }
 end
