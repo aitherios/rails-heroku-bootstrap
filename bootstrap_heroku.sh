9fdf1005ad639f326f752dc098fdfd0457f44451
Renan Mendes Carvalho <aitherios@gmail.com>
2013-04-20 23:23:00 -0300

    initializing app_hostname in heroku bootstrap

--- bootstrap_heroku.sh
+++ bootstrap_heroku.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-heroku addons:add heroku-postgresql
+heroku addons:add heroku-postgresql:dev
 heroku addons:add newrelic:standard
 heroku addons:add pgbackups:auto-month
 heroku addons:add redistogo:nano
@@ -8,3 +8,5 @@ heroku addons:add sendgrid:starter
 heroku addons:add sentry:developer
 heroku addons:add zerigo_dns:basic
 heroku addons:add logentries:tryit
+
+heroku config:add APP_HOSTNAME=`heroku domains |grep 'herokuapp.com'`
