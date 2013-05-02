33f15c6b68dd66103f5c5a59159c0e7a261a8f82
Renan Mendes Carvalho <aitherios@gmail.com>
2013-05-02 14:45:42 -0300

    configuring redactor uploaders to use the project default

--- app/uploaders/redactor_rails_document_uploader.rb
+++ app/uploaders/redactor_rails_document_uploader.rb
@@ -2,11 +2,8 @@
 class RedactorRailsDocumentUploader < CarrierWave::Uploader::Base
   include RedactorRails::Backend::CarrierWave
 
-  # storage :fog
-  storage :file
-
   def store_dir
-    "system/redactor_assets/documents/#{model.id}"
+    "uploads/redactor_assets/documents/#{model.id}"
   end
 
   def extension_white_list
--- app/uploaders/redactor_rails_picture_uploader.rb
+++ app/uploaders/redactor_rails_picture_uploader.rb
@@ -7,13 +7,10 @@ class RedactorRailsPictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   # include CarrierWave::ImageScience
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "system/redactor_assets/pictures/#{model.id}"
+    "uploads/redactor_assets/pictures/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
