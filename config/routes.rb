b7ad9995448452a6d38c4e93480da8328f7b9108
Renan Mendes Carvalho <aitherios@gmail.com>
2013-05-31 13:07:26 -0300

    removing redactor-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 require 'resque/server'
 
 RailsHerokuBootstrap::Application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
-  mount RedactorRails::Engine => '/redactor_rails'
   mount Mercury::Engine => '/'
+  mount Ckeditor::Engine => '/ckeditor'
   mount Kss::Engine => '/kss' if Rails.env.development?
 
   devise_for :admin_users, ActiveAdmin::Devise.config
